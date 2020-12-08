e162374b174a9688a33a60f0c2600293e3b13737
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2020-12-08 12:17:39 +0300

    Explicitly use an Instant from iOS code

--- shared/src/iosMain/kotlin/com/example/kmmapp/shared/Platform.kt
+++ shared/src/iosMain/kotlin/com/example/kmmapp/shared/Platform.kt
@@ -2,6 +2,9 @@ package com.example.kmmapp.shared
 
 
 import platform.UIKit.UIDevice
+import kotlinx.datetime.*
+
+val instant = Clock.System.now()
 
 actual class Platform actual constructor() {
     actual val platform: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
